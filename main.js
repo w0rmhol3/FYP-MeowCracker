f8d30c2c8d20de8ea62d33e9861f1d9a852b2a94
Kelvin Teo <k3lvintwl2001@gmail.com>
2023-05-26 16:56:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function update_command(){
   var wordlist = document.getElementById('Wordlist').value;
   const outputElement = document.getElementById('Straight-Attack');
   
-  outputElement.textContent = "HashCat -a 0 -m "+ hash_type + file_input + " -o StraightAttack.txt " + wordlist;
+  outputElement.textContent = "HashCat -a 0 -m "+ hash_type + " " + file_input + " -o StraightAttack.txt /usr/share/wordlists/" + wordlist;
 }
 
 //Function to switch the tabs within the website
