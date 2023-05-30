54483f490e363ddb22dbd22d5ee637fa0f665b72
Kelvin Teo <k3lvintwl2001@gmail.com>
2023-05-30 20:02:40 +0800

    add a json and do testing for dropdownlist with js

--- main.js
+++ main.js
@@ -16,6 +16,56 @@ function SwitchMode() {
 
 }
 
+function HashesDropdown()
+{
+
+  let dropdown = document.getElementById('HashTypeDropdown2');
+  dropdown.length = 0;
+  
+  let defaultOption = document.createElement('option');
+  defaultOption.text = 'Choose a hash';
+  
+  dropdown.add(defaultOption);
+  dropdown.selectedIndex = 0;
+  
+  const url = 'https://w0rmhol3.github.io/Hashes.json';
+  
+  fetch(url)  
+    .then(  
+      function(response) {  
+        if (response.status !== 200) {  
+          console.warn('Looks like there was a problem. Status Code: ' + 
+            response.status);  
+          return;  
+        }
+  
+        // Examine the text in the response  
+        response.json().then(function(data) {  
+          let option;
+      
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option');
+            option.text = data[i].name;
+            option.value = data[i].abbreviation;
+            dropdown.add(option);
+        }    
+        });  
+      }  
+    )  
+    .catch(function(err) {  
+      console.error('Fetch Error -', err);  
+    });
+}
+
+
+
+
+
+
+
+
+
+
 //The function to generate the command on Straight Attack 
 function update_command(){
   console.log("Reach");
